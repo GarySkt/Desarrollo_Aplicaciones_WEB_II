1069eeea2c75d9d99f4d20553cf66afaaf76ee31
Gary Calle <joevermelho@protonmail.com>
2018-09-04 12:30:34 -0500

    Ejercicio_05
    anhadido ejercicio 05 con un touch de disenho

---
+++ Ejercicio_05/Ejercicio_05/Controllers/SubsidioController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejercicio_05.Models;
+
+namespace Ejercicio_05.Controllers
+{
+    public class SubsidioController : Controller
+    {
+        // GET: Subsidio
+        public static double viuda = 20;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CalcularSubsidio(ClsSubsidio objsubsidio)
+        {
+            if (objsubsidio.cant_hijos == "0 - 2")
+            {
+                objsubsidio.resultado = 70 + (objsubsidio.hijos_escolaridad * 10);
+            }
+            if(objsubsidio.cant_hijos == "3 - 5")
+            {
+                objsubsidio.resultado = 90 + (objsubsidio.hijos_escolaridad * 10);
+            }
+            if (objsubsidio.cant_hijos == "+ 6")
+            {
+                objsubsidio.resultado = 120 + (objsubsidio.hijos_escolaridad * 10);
+            }
+            if (objsubsidio.viuda == true)
+            {
+                objsubsidio.resultado = objsubsidio.resultado + viuda;
+            }
+
+            
+
+            return View(objsubsidio);
+        }
+        public ActionResult EnviarDatos(ClsSubsidio objsubsidio)
+        {
+            return View();
+        }
+    }
+}
---
+++ Ejercicio_05/Ejercicio_05/Models/ClsSubsidio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejercicio_05.Models
+{
+    public class ClsSubsidio
+    {
+        public string cant_hijos { get; set; }
+        public int hijos_escolaridad { get; set; }
+
+        public bool viuda { get; set; }
+        public double resultado { get; set; }
+    }
+}
