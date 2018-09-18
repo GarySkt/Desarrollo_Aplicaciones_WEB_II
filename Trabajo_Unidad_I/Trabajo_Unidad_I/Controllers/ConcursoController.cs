43e9f4b49dbbea161261ac10606b8bf630e6a11f
Gary Calle <joevermelho@protonmail.com>
2018-09-17 19:37:27 -0500

    ejercicios propuestos mas trabajo de unidad (ejer_2)

---
+++ Trabajo_Unidad_I/Trabajo_Unidad_I/Controllers/ConcursoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trabajo_Unidad_I.Controllers
+{
+    public class ConcursoController : Controller
+    {
+        // GET: Concurso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Trabajo_Unidad_I/Trabajo_Unidad_I/Models/ClsCore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trabajo_Unidad_I.Models
+{
+    public class ClsCore
+    {
+        public DateTime parallelStart { get; set; }
+        public DateTime parallelFinish { get; set; }
+        public DateTime singeStart { get; set; }
+        public DateTime singeFinish { get; set; }
+
+        public int parallel_execution_time { get; set; }
+        public int Singe_execution_time { get; set; }
+
+    }
+}
